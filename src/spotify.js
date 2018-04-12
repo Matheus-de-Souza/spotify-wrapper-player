88106031d3f7449e0f8fe8a9157603d911e09dd4
Matheus de Souza <mws.souza@gmail.com>
2018-04-12 16:22:06 -0300

    Import spotify wrapper lib

---
+++ src/spotify.js
@@ -0,0 +1,8 @@
+
+import SpotifyWrapper from 'mws-spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'token',
+});
+
+export default spotify;
---
+++ tests/spotify.spec.js
@@ -0,0 +1,24 @@
+/* globals describe it */
+
+import chai, { expect } from 'chai';
+import dirtyChai from 'dirty-chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+
+import spotify from '../src/spotify';
+
+chai.use(sinonChai);
+chai.use(dirtyChai);
+
+describe('Spotify', () => {
+  it('should be an object', () => {
+    expect(spotify).to.be.an('object');
+  });
+  it('should have method search', () => {
+    expect(spotify.search).to.be.exist();
+  });
+  it('should have method album', () => {
+    expect(spotify.album).to.be.exist();
+  });
+});
