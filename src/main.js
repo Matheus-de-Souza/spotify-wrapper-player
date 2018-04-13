91afa13e2840626b6577aacff4cf9ba4f3e9bfef
Matheus de Souza <mws.souza@gmail.com>
2018-04-13 09:35:26 -0300

    Render a real album list

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 
-console.log('Spotify Wrapper');
+
+import AlbumList from './AlbumList';
+import spotify from './Spotify';
+import renderAlbums from './AlbumList';
+
+const albums = spotify.search.albums('Vinnie Moore');
+const albumList = document.getElementById('album-list');
+
+albums.then(console.log);
+albums.then(data => console.log(data.albums.items));
+albums.then(data => renderAlbums(data.albums.items, albumList));
