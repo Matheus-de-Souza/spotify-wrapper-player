c54a210c6c0df94012e145a75e929cef3e81a981
Matheus de Souza <mws.souza@gmail.com>
2018-04-19 14:33:55 -0300

    Refactoring

--- src/SearchTrigger.js
+++ src/SearchTrigger.js
@@ -5,10 +5,14 @@ const searchInput = document.getElementById('search-input');
 const albumList   = document.getElementById('album-list');
 const searchForm = document.getElementById('search-form');
 
+function makeRequest(query) {
+  spotify.search.albums(query)
+    .then(data => renderAlbums(data.albums.items, albumList));
+}
+
 export default function searchEnterTrigger() {
   searchForm.addEventListener('submit', (e) => {
     e.preventDefault(); // Evitar request na página
-    spotify.search.albums(searchInput.value)
-      .then(data => renderAlbums(data.albums.items, albumList));
+    makeRequest(searchInput.value);
   });
 }
--- src/SelectAlbumTrigger.js
+++ src/SelectAlbumTrigger.js
@@ -6,10 +6,15 @@ const albumList   = document.getElementById('album-list');
 const albumInfo   = document.getElementById('album-info');
 const albumTracks = document.getElementById('album-tracks');
 
+function makeRequest(albumId) {
+  spotify.album.getAlbum(albumId)
+    .then(data => renderAlbumInfo(data, albumInfo))
+    .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+}
+
 export default function selectAlbumTrigger() {
   albumList.addEventListener('click', (e) => {
-    spotify.album.getAlbum(e.target.getAttribute('data-album-id'))
-      .then(data => renderAlbumInfo(data, albumInfo))
-      .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
+    const target = e.target;
+    makeRequest(target.getAttribute('data-album-id'));
   });
 }
